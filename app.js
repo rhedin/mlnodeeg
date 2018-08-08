c0dae7a06ff0ede854245d96272df6fd2d0dee82
Rick Hedin <cubsno1@gmail.com>
2018-08-08 18:11:29 -0400

    Saving work.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var load2docsRouter = require('./routes/load2docs');
+var docsRouter = require('./routes/docs');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/load2docs', load2docsRouter);
+app.use('/docs', docsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
