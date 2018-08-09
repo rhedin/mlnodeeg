0c2a16f2946f63b9075cd8b5e4e2f29ee9da76e8
Rick Hedin <cubsno1@gmail.com>
2018-08-08 22:23:01 -0400

    Now can add 'em and remove 'em.

--- routes/docs.js
+++ routes/docs.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var marklogic = require('marklogic');
 var newsData = require('./data.js');
 
-function load2docs(callback) {
+function storeDocs(callback) {
     var db = marklogic.createDatabaseClient({
         host:     'localhost',
         port:     '8000',
@@ -12,28 +12,52 @@ function load2docs(callback) {
         password: 'Sadie1Tink2',
         authType: 'DIGEST'
     });
-    // Wouldn't it be nice to find out this failed now, at the 
-    // time we create the database client.  Currently, we find 
-    // out when we try to use it.
     
     db.createCollection(
-        '/books',
-        {author: 'Beryl Markham'},
-        {author: 'WG Sebald'}
+        '/news',
+        newsData.articles
     )
     .result(function(response) {
-        console.log(`Call to db.createCollection succeeded.\n${JSON.stringify(response, null, 2)}`);
+        console.log(`Call to db.createCollection succeeded.\n${JSON.stringify(response, null, 4)}`);
         callback();
     }, function (error) {
-        console.log(`Call to db.createCollection failed.\n${JSON.stringify(error, null, 2)}`);
+        console.log(`Call to db.createCollection failed.\n${JSON.stringify(error, null, 4)}`);
         callback();  // Calls back just the same if there is an error.
     });
-    // What happens if I create the collection twice?
-    // Ans: It just succeeds, but is much faster!
+}
+
+function removeDocs(callback) {
+    var db = marklogic.createDatabaseClient({
+        host:     'localhost',
+        port:     '8000',
+        database: 'Documents',
+        user:     'rickhedin',
+        password: 'Sadie1Tink2',
+        authType: 'DIGEST'
+    });
+    
+    db.documents.removeAll({collection: '/news'})
+    .result(function(response) {
+        console.log(`Call to db.removeAll succeeded.\n${JSON.stringify(response, null, 4)}`);
+        callback();
+    }, function (error) {
+        console.log(`Call to db.removeAll failed.\n${JSON.stringify(error, null, 4)}`);
+        callback();
+    });
 }
 
 router.get('/add', function(req, res, next) {
     console.log(`In docs/add route.`);
+    storeDocs( () => {
+        res.send('Stored the documents.');
+    });
+});
+
+router.get('/remove', function(req, res, next) {
+    console.log(`In docs/remove route.`);
+    removeDocs( () => {
+        res.send('Removed the documents.');
+    });
 });
 
 module.exports = router;
@@ -42,5 +66,9 @@ module.exports = router;
 /***
 
 This should broken into one file per operation, but oh well.
+
+Shouldn't use "get" for everything.
+
+Really?  Going to get a new database connection each time? 
 
 ***/
